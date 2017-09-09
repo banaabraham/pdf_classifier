d6e8cb48c3ef9962524aeff0af9f7c13b8524daf
Bana Abraham <bana.abraham@gmail.com>
2017-09-09 20:54:00 +0700

    Update pdf_class.py

--- pdf_class.py
+++ pdf_class.py
@@ -6,50 +6,63 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
-from threading import Thread
 
 
 #convert pdf file to text function
-def convert(fname):
-    pagenums = set()
-    output = StringIO()
-    manager = PDFResourceManager()
-    converter = TextConverter(manager, output, laparams=LAParams())
-    interpreter = PDFPageInterpreter(manager, converter)
-    
-    infile = open(fname, 'rb')
-    for page in PDFPage.get_pages(infile, pagenums):
-        interpreter.process_page(page)
-    infile.close()
-    converter.close()
-    text = output.getvalue()
-    output.close
-    return text 
 
-def find_text(query,text,result_dict):
-    text_list = str(text.decode("utf8")).split(" ")
-    count = 0
-    for i in text_list:
-        if i==query:
-            count+=1
-    result_dict[query] = count        
+class pdf_classify(object):
     
-#classify the pdf using machine learning algorithm
-def classification(class_dict):    
-    data = np.array([i for i in class_dict.values()][::-1]).reshape(1,-1)
-    x = np.array([[0,0,0,41,0],[4,1,0,0,1],[1,1,0,0,5]])
-    Y = np.array([1,2,2])                        
-    gnb = GaussianNB()
-    gnb.fit(x,Y)
-    return int(gnb.predict(data))
-
-def classify(classification):
-    if classification == 1:
-        print("SOCIAL")
-    elif classification == 2:
-        print("SCIENCE")
+    def __init__(self,fname):
+        self.fname = fname
+        self.keyword_english = ["computer","economy","religion","social","technology"]
+        self.result_dict = dict()
+        #training data, it's a count for each keyword in keyword_english
+        self.x = np.array([[0,0,0,41,0],[4,1,0,0,1],[1,1,0,0,5]])
+        self.Y = np.array([1,2,2])
+    
+    #convert pdf to text 
+    def convert(self):
+        pagenums = set()
+        output = StringIO()
+        manager = PDFResourceManager()
+        converter = TextConverter(manager, output, laparams=LAParams())
+        interpreter = PDFPageInterpreter(manager, converter)        
+        infile = open(self.fname, 'rb')
+        for page in PDFPage.get_pages(infile, pagenums):
+            interpreter.process_page(page)
+        infile.close()
+        converter.close()
+        self.text = output.getvalue()
+        output.close
+        return self.text  
         
-def get_list(directory):
+    def find_all(self):
+        self.convert()
+        text_list = str(self.text.decode("utf8")).split(" ")
+        for query in self.keyword_english:
+            count = 0
+            for i in text_list:
+                if i==query:
+                    count+=1
+            self.result_dict[query] = count  
+                              
+    #classify the pdf using machine learning algorithm
+    def classify(self):
+        self.find_all()
+        data = np.array([i for i in self.result_dict.values()][::-1]).reshape(1,-1)
+                                
+        gnb = GaussianNB()
+        gnb.fit(self.x,self.Y)
+        self.prediction = int(gnb.predict(data))
+        
+        if self.prediction == 1:
+            print("%s is SOCIAL" %(self.fname))
+        elif self.prediction == 2:
+            print("%s is SCIENCE" %(self.fname))
+        return self.prediction,data,self.result_dict
+        
+                   
+def get_list(directory=os.getcwd()):
     pdflist=[]
     for filename in os.listdir(directory):
         if filename.endswith(".pdf"): 
@@ -60,21 +73,6 @@ def get_list(directory):
             continue
     return pdflist 
 
-
 if __name__ == "__main__":
-    pdf_name = input("input pdf file name: ")
-    text = convert(pdf_name)
-    keyword_english = ["economy","social","technology","religion","computer"]
-    
-    class_dict = dict()
-    
-    threads = []
-    for i in keyword_english:
-        find_text(i,text,class_dict)
-        t = Thread(target=find_text,args=(i,text,class_dict))
-        threads.append(t)
-        t.start()
-    for t in threads:
-        t.join()    
-                
-    classify(classification(class_dict))
+    ebook = pdf_classify("something.pdf")
+    ebook.classify()
